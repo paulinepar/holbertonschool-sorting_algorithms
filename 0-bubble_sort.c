fdd01c7b52ec47caf596ab1f0f42def5ba6cfc37
paulinepar <paulineparmigiani@gmail.com>
2023-01-02 17:40:53 +0100

    Task 0 modif correction NULL

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, j = 0;
 	int tmp;
 
+	if (array == NULL)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < (size - i - 1); j++)
