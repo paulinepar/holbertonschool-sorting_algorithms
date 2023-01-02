e679504ba7ebf1c0c19c53358599aeb2af53cc5e
Meashyshy <camillefvrl@gmail.com>
2023-01-02 07:19:09 -0800

    Deleted mistake

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,22 +10,4 @@ void insertion_sort_list(listint_t **list)
 {
 	if (!list || !*list || !(*list)->next)
 		return;
-
-	listint_t *currentNode = (*list)->next;
-
-	while (currentNode)
-	{
-		listint_t *previousNode = currentNode->prev;
-		int tmp = currentNode->n;
-
-		while (previousNode && previousNode->n > tmp)
-		{
-			previousNode->next->n = previousNode->n;
-			print_list(*list);
-			previousNode->n = tmp;
-			previousNode = previousNode->prev;
-		}
-
-	currentNode = currentNode->next;
-}
 }
