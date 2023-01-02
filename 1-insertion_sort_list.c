f1822cf34c6b5b982c5b64c102ec4f423f8f0fd8
Meashyshy <camillefvrl@gmail.com>
2023-01-02 07:15:51 -0800

    Swaps with the previous element

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -15,7 +15,17 @@ void insertion_sort_list(listint_t **list)
 
 	while (currentNode)
 	{
+		listint_t *previousNode = currentNode->prev;
 		int tmp = currentNode->n;
-	}
+
+		while (previousNode && previousNode->n > tmp)
+		{
+			previousNode->next->n = previousNode->n;
+			print_list(*list);
+			previousNode->n = tmp;
+			previousNode = previousNode->prev;
+		}
+
 	currentNode = currentNode->next;
+}
 }
