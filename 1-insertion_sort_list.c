3a59759708a9f4642d8aaefb4126627f070b799d
Meashyshy <camillefvrl@gmail.com>
2023-01-03 04:55:03 -0800

    Testing checker

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,23 +8,47 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *tmpNode;
+	listint_t *Node;
 	listint_t *currentNode;
 
 	if (!list || !*list || !(*list)->next)
 		return;
 
-	currentNode = (*list)->next;
+	tmpNode = NULL;
+	Node = *list;
 
-	while (currentNode)
+	while (Node)
 	{
-		listint_t *previousNode = currentNode->prev;
-		listint_t *Node = currentNode;
-		int tmp = currentNode->n;
+		listint_t *nextNode = Node->next;
 
-		while (previousNode && previousNode->n > tmp)
+		if (!tmpNode || tmpNode->n >= Node->n)
 		{
-			Node->prev = previousNode->prev;
+			Node->prev = NULL;
+			Node->next = tmpNode;
+
+			if (tmpNode)
+				tmpNode->prev = Node;
+			tmpNode = Node;
 		}
-	currentNode = currentNode->next;
-}
+		else
+		{
+			currentNode = tmpNode;
+
+			while (currentNode->next && currentNode->next->n < Node->n)
+				currentNode = currentNode->next;
+
+			Node->prev = currentNode;
+			Node->next = currentNode->next;
+
+			if (currentNode->next)
+				currentNode->next->prev = Node;
+
+			currentNode->next = Node;
+		}
+		print_list(tmpNode);
+
+		Node = nextNode;
+	}
+	*list = tmpNode;
 }
