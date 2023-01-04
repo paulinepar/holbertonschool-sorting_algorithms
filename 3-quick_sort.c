0ddc38b7d1dce827e678a6b87f0f58146d848ca1
Meashyshy <camillefvrl@gmail.com>
2023-01-04 09:48:34 -0800

    Fixed Betty Style

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,7 +7,8 @@
  *
  * Return: 0
  */
-void quick_sort(__attribute__((unused)) int *array, __attribute__((unused)) size_t size)
+void quick_sort(__attribute__((unused)) 
+		int *array, __attribute__((unused)) size_t size)
 {
 	if (size < 1)
 		return;
