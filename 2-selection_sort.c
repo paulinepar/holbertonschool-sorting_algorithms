c4fd2fd65e10a4026c834ef4503ed2b08b00870b
Meashyshy <camillefvrl@gmail.com>
2023-01-04 09:06:58 -0800

    Testing Kyllian code

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ *swap - swap
+ *@x:x
+ *@y:y
+ */
+
+void swap(intx, int y)
+{
+    int temp = *x;
+    x =y;
+    *y = temp;
+}
+
+
+/**
+ *selection_sort - selection sort
+ *@arr: array
+ *@n: n
+ */
+
+void selection_sort(int arr[], int n)
+{
+        int i, j, k;
+    for (i = 0; i < n-1; i++)
+    {
+        k = i;
+        for (j = i+1; j < n; j++)
+          if (arr[j] < arr[k])
+            k = j;
+
+           if(k != i)
+            swap(&arr[k], &arr[i]);
+    }
+}
