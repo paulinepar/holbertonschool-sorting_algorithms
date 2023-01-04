21bb2cda2b36ab6a86283f13cbc0eb7cfc16a852
Meashyshy <camillefvrl@gmail.com>
2023-01-04 09:18:41 -0800

    Added pointers before variables and braces

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,10 +9,9 @@
 void swap(int x, int y)
 {
     int temp = *x;
-    x =y;
+    *x = *y;
     *y = temp;
 }
-
 
 /**
  *selection_sort - selection sort
@@ -22,15 +21,21 @@ void swap(int x, int y)
 
 void selection_sort(int arr[], int n)
 {
-        int i, j, k;
-    for (i = 0; i < n-1; i++)
-    {
-        k = i;
-        for (j = i+1; j < n; j++)
-          if (arr[j] < arr[k])
-            k = j;
+	int i, j, k;
+	for (i = 0; i < n-1; i++)
+	{
+		k = i;
+		for (j = i+1; j < n; j++)
+		{
+			if (arr[j] < arr[k])
+			{
+				k = j;
+			}
+		}
 
-           if(k != i)
-            swap(&arr[k], &arr[i]);
-    }
+		if(k != i)
+		{
+			swap(&arr[k], &arr[i]);
+		}
+	}
 }
