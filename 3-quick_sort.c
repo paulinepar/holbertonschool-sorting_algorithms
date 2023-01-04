7c53ba390ce1fdd6299b32412f9c8bfa09940534
Meashyshy <camillefvrl@gmail.com>
2023-01-04 09:43:38 -0800

    Testing Checker

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * quick_sort - function that sorts an array of integers in ascending
+ * order using the quick sort algorithm.
+ * @array: array
+ * @size: size
+ *
+ * Return: 0
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size < 1)
+		return;
+}
